1970c3e2841d6221b9a84ba3d30815427dfd1ab5
xso <124799143+xsoga@users.noreply.github.com>
2023-06-07 12:54:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ async def process_record_command(message: types.Message):
     @dp.message_handler(lambda message: message.text in ["Терапевт", "Гастроэнтеролог ", "Хирург"])
     async def process_doctor_choice(message: types.Message):
         doctor = message.text
-
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"])
         await message.reply("Выберите дату:", reply_markup=keyboard)
@@ -32,7 +31,6 @@ async def process_record_command(message: types.Message):
         @dp.message_handler(lambda message: message.text in ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"])
         async def process_date_choice(message: types.Message):
             date = message.text
-
             keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
             keyboard.add(*["10:00", "11:00", "12:00", "13:00", "14:00"])
             await message.reply("Выберите время:", reply_markup=keyboard)
@@ -41,14 +39,11 @@ async def process_record_command(message: types.Message):
             @dp.message_handler(lambda message: message.text in ["10:00", "11:00", "12:00", "13:00", "14:00"])
             async def process_time_choice(message: types.Message):
                 time = message.text
-
-
                 await message.reply("Введите ФИО пациента:")
 
                 @dp.message_handler()
                 async def process_patient_name(message: types.Message):
                     patient_name = message.text
-
                     await message.reply(f"Вы записались к {doctor} на {date} в {time}. ФИО пациента: {patient_name}")
 
 if __name__ == '__main__':
